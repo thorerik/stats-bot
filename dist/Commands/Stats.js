a747426e6048055bf942d9a26ab9b29bdda302db
Thor Erik Lie <thor.erik.lie@basefarm.com>
2018-01-27 18:36:10 +0100

    basic stats command

---
+++ dist/Commands/Stats.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const GuildConfiguration_1 = require("../Database/Models/GuildConfiguration");
+const Stats_1 = require("../Database/Models/Stats");
+const Properties_1 = require("../Lib/Properties");
+class Stats {
+    constructor() {
+        this.help = "Gets you some stats for current guild";
+        this.examples = [
+            "stats",
+        ];
+        this.permissionRequired = discord_js_1.Permissions.FLAGS.SEND_MESSAGES;
+        this.props = Properties_1.Properties.getInstance();
+    }
+    async run(message, args) {
+        const guildConfiguration = await GuildConfiguration_1.GuildConfiguration.findOne({ where: { guildID: message.guild.id.toString() } });
+        const guildConfig = JSON.parse(guildConfiguration.settings);
+        const stats = await Stats_1.Stats.findAll({
+            limit: 10,
+            order: [
+                ["count", "DESC"],
+            ],
+            where: { guildID: message.guild.id.toString() },
+        });
+        const embed = new discord_js_1.MessageEmbed();
+        embed.color = discord_js_1.Util.resolveColor([0, 255, 255]);
+        embed.thumbnail = { url: "https://i.imgur.com/ibsHxIR.png" };
+        embed.title = "Top 10 spammers";
+        embed.author = {
+            iconURL: "https://i.imgur.com/ibsHxIR.png",
+            name: "Statsbot",
+        };
+        embed.footer = {
+            text: "Created by Tuxy Fluffyclaws#1337",
+        };
+        embed.description = "";
+        stats.forEach((record) => {
+            embed.description += `<@${record.userId}>: ${record.count}`;
+            embed.description += "\n";
+        });
+        message.channel.send({ embed });
+    }
+}
+exports.Stats = Stats;
---
+++ src/Commands/Stats.ts
@@ -0,0 +1,56 @@
+import { Message, MessageEmbed, Permissions, Util, User } from "discord.js";
+
+import * as log from "fancy-log";
+
+import { GuildConfiguration } from "../Database/Models/GuildConfiguration";
+import { Stats as DbStats } from "../Database/Models/Stats";
+import { Command } from "../Lib/Command";
+import { Properties } from "../Lib/Properties";
+
+export class Stats implements Command {
+    public help = "Gets you some stats for current guild";
+    public examples = [
+        "stats",
+    ];
+    public permissionRequired = Permissions.FLAGS.SEND_MESSAGES;
+    private props = Properties.getInstance();
+
+    public async run(message: Message, args: string[]) {
+        const guildConfiguration = await GuildConfiguration.findOne({where: {guildID: message.guild.id.toString()}});
+        const guildConfig = JSON.parse(guildConfiguration.settings);
+
+        const stats = await DbStats.findAll({
+            limit: 10,
+            order: [
+                ["count", "DESC"],
+            ],
+            where: {guildID: message.guild.id.toString()},
+        });
+
+        const embed = new MessageEmbed();
+
+        embed.color = Util.resolveColor([0, 255, 255]);
+
+        embed.thumbnail = {url: "https://i.imgur.com/ibsHxIR.png"};
+
+        embed.title = "Top 10 spammers";
+
+        embed.author = {
+            iconURL: "https://i.imgur.com/ibsHxIR.png",
+            name: "Statsbot",
+        };
+
+        embed.footer = {
+            text: "Created by Tuxy Fluffyclaws#1337",
+        };
+
+        embed.description = "";
+
+        stats.forEach((record) => {
+            embed.description += `<@${record.userId}>: ${record.count}`;
+            embed.description += "\n";
+        });
+
+        message.channel.send({embed});
+    }
+}
